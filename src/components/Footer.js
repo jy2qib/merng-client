0f56ce55de1c1fc8d271f533ae28aebdba6d8e90
jy2qib <jy2qib@yahoo.com>
2020-02-25 10:55:10 +0800

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import sniffyRev from '../img/sniffy-rev.png';
 
 function Footer() {
         const variable = "";
     return (
         <div class="footer">
+            <div class="footer-left">
             <p><strong>J Tuquib</strong> - Web &amp; Graphics Developer<br />
-            <span>Tgrafix Design &amp; Development Laboratory - Copyright &copy; 2020</span></p>
+            <span>Tgrafix - A Design &amp; Development Laboratory</span></p>
+            </div>
+            <div class="footer-right">
+                <img src={ sniffyRev } alt="I am a coder!" width="90px" />
+                <p><span>Copyright &copy; 2020. &nbsp;&nbsp;</span> <span>All rights reserved.</span></p>
+            </div>
         </div>
     )
 }
