19ad20b61841114ece40433f76dd4f2885a7d7ad
jy2qib <jy2qib@yahoo.com>
2020-02-25 10:34:05 +0800

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
         const variable = "";
     return (
         <div class="footer">
-            <p>J Tuquib - Web &amp; Graphics Developer<br />
+            <p><strong>J Tuquib</strong> - Web &amp; Graphics Developer<br />
             <span>Tgrafix Design &amp; Development Laboratory - Copyright &copy; 2020</span></p>
         </div>
     )
